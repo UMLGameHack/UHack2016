81244420fbf1a160633dc02792594971a7117456
Andy MacGregor <Andrew_MacGregor@student.uml.edu>
2016-10-08 03:38:29 -0400

    index on master: b68c3ae Music in Victory Menu

---
+++ UHackGame/Assets/Sprites/Scripts/MenuMaster.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuMaster : MonoBehaviour {
+
+	public void goLevel1(){
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("Level1");
+	}
+
+	public void quitGame(){
+		Application.Quit ();
+	}
+	
+}
---
+++ UHackGame/Assets/Sprites/Scripts/MicrophoneInput.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MicrophoneInput : MonoBehaviour
+{
+    private string myMicrophone; //the microphone we will be using.
+    private const int FREQ_RATE = 5000; //sampling rate... up as needed.
+    private const double SAMPLE_DURATION = 0.04; //duration of each sample.
+    // Use this for initialization
+    void Start()
+    {
+        myMicrophone = null;
+        Debug.Log("MicrophoneInput has been Started");
+        foreach (string device in UnityEngine.Microphone.devices)
+        {
+            //stores the first microphone found as myMicrophone (may be problematic)
+            if (myMicrophone == null)
+                myMicrophone = device;
+            Debug.Log("Name: " + device);
+        }
+    }
+
+    void Update()
+    {
+     
+    }
+
+}
